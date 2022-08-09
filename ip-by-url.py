456bade75b213afdcddcc4c83a2940862e927485
Fsociety-MrRobot <110719206+Fsociety-MrRobot@users.noreply.github.com>
2022-08-09 22:25:49 +0300

    Update ip-by-url.py

--- ip-by-url.py
+++ ip-by-url.py
@@ -1,5 +1,4 @@
 import socket
-import pyfiglet
 from colorama import Fore, Back, Style
 
 
@@ -19,9 +18,6 @@ def get_ipaddr_by_hostname(hostname):
 		
 
 def main():
-	preview = pyfiglet.figlet_format('PBRL Tool', font = 'isometric1')
-	print(f'{preview}\n ')
-	
 	hostname = input('Please enter a website address (URL): ')
 	print(get_ipaddr_by_hostname(hostname))
 
